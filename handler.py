8ecef0c0d2ad0f6b0901046310aba44a6fa48686
Max Strömberg <m@maxstr.se>
2020-10-04 15:10:58 +0200

    change lambda timeout and add some comments

--- handler.py
+++ handler.py
@@ -12,11 +12,11 @@ FRAMEIO_TOKEN = 'your_token'
 s3 = boto3.resource('s3')
 lambda_client = boto3.client('lambda', config=Config(region_name='us-east-1'))
 frameio_client = frameioclient.FrameioClient(FRAMEIO_TOKEN)
-LAMBDA_TIMEOUT = 2
+LAMBDA_TIMEOUT = 720    # 12 minutes
 
 
 class Asset:
-    def __init__(self, name, path, size, parent_asset_id, bucket_name, is_file=True):
+    def __init__(self, name, path, size, parent_asset_id, bucket_name, is_file):
         self.name = name
         self.path = path
         self.size = size
@@ -53,6 +53,7 @@ def upload_file(file):
 
 
 def project_root_asset_id(project_name):
+    """Translate project name into project root_asset_id"""
     projects = []
 
     for team in frameio_client.get_all_teams():
@@ -73,6 +74,7 @@ def lambda_timeout(start_time):
 
 
 def copy(event, context):
+    """Copy files and folders from S3 to Frame.io. Restart if Lambda timeouts"""
     bucket_name = event['bucket_name']
     previous_asset = event['previous_asset']
     continued_run = event['continued_run']
@@ -85,13 +87,11 @@ def copy(event, context):
     for object in objects:
         # If this lambda continues where another left of, first iterate down to the last uploaded file.
         if continued_run == 'true':
-            if object.key != previous_asset:
-                continue
-            else:
+            if object.key == previous_asset:
                 continued_run = 'false'
-                continue
+            continue
 
-        # Delete finished folder from dict to keep size down.
+        # Delete finished folder from parent_ids dict to keep size down.
         current_folder = os.path.dirname(object.key[:-1])
         if len(current_folder) < len(previous_folder):     # We've moved back up in folder structure
             del parent_ids[previous_folder]
@@ -105,7 +105,7 @@ def copy(event, context):
                     'bucket_name': bucket_name,
                     'previous_asset': previous_asset,
                     'continued_run': 'true',
-                    'parent_ids': parent_ids})  # Dict with all folder - asset_id pairs. '' is root.
+                    'parent_ids': parent_ids})  # Dict with folder:asset_id pairs. '' is root.
             )
 
             return
@@ -149,12 +149,13 @@ def copy(event, context):
 
 
 def main(event, context):
+    """Validate POST request and trigger Lambda copy function."""
     try:
         body = json.loads(event['body'])
 
         bucket = body['bucket']
         project = body['project']
-        frameio_token = body['frameio_token']
+        token = body['token']
 
     except (TypeError, KeyError):
         return {
@@ -163,7 +164,7 @@ def main(event, context):
         }
 
     # Simple authentication
-    if not frameio_token == FRAMEIO_TOKEN:
+    if not token == FRAMEIO_TOKEN:
         return {
             "statusCode": 401,
             "body": json.dumps({"message": "Unauthorized"})
@@ -193,7 +194,7 @@ def main(event, context):
             'bucket_name': bucket,
             'previous_asset': '',
             'continued_run': 'false',
-            'parent_ids': {'': root_asset_id}})     # Dict with all folder -> asset_id pairs. '' is root.
+            'parent_ids': {'': root_asset_id}})     # Dict with folder:asset_id pairs. '' is root.
     )
 
     return {
